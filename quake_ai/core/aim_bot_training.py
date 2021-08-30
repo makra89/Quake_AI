56d936606e3c46199f4c1036cd2ec74a01aad5ab
Manuel Kraus <manuel.mk.kraus@gmail.com>
2021-08-30 12:35:05 +0200

    Implement automatic annotation

--- quake_ai/core/aim_bot_training.py
+++ quake_ai/core/aim_bot_training.py
@@ -32,6 +32,10 @@ import os
 import keyboard
 import numpy as np
 import re
+from PIL import Image
+import cv2 as cv
+
+from quake_ai.utils.model_helpers import TriggerModel
 
 
 class AimbotTrainer:
@@ -82,6 +86,133 @@ class AimbotTrainer:
         print("Current number of images:", self._curr_image_inc)
 
         image.save(os.path.join(self._image_path, str(self._curr_image_inc) + '.png'))
+
+
+class ImageAnnotator:
+    """ Automatically annotates images for the aimbot training by
+        sliding the triggerbot fov over an entire image.
+        For every image in the path it will create a <image_name>_anno.txt file
+    """
+
+    def __init__(self, config):
+        """ Initialize the annotator, uses the path to the trained trigger model """
+
+        self._config = config
+        self._image_path = os.path.join(self._config.training_env_path, 'aimbot_images')
+        self._annotated_image_path = os.path.join(self._image_path, 'annotated_images')
+        self._model_path = config.trigger_model_path
+        self._trigger_fov = (config.trigger_fov[0], config.trigger_fov[1])
+        self._aimbot_training_fov = config.aimbot_train_image_fov
+        self._step_size_height = config.annotator_step_size_height
+        self._step_size_width = config.annotator_step_size_width
+        # Do not do it here, prevent tensorflow from loading
+        self._model = None
+        self._file_list = None
+        self._current_image_idx = None
+        self._num_images_to_process = config.annotator_num_images_per_step
+
+    def startup_annotation(self):
+        """ Initialize the trigger model for annotation """
+
+        # Check if path for images already exists
+        if not os.path.exists(self._annotated_image_path):
+            os.makedirs(self._annotated_image_path)
+
+        # Only initialize once!
+        if self._model is None:
+            self._model = TriggerModel(self._config, self._trigger_fov, self._model_path)
+
+        # Now collect all images(!) that do not have an annotation file yet
+        self._file_list = []
+        self._current_image_idx = 0
+        file_list = os.listdir(self._image_path)
+        for file in file_list:
+            is_image = '.png' in file
+            anno_exists = is_image and os.path.isfile(os.path.join(self._image_path,
+                                                                   file.split('.')[0] + "_anno.txt"))
+
+            if is_image and not anno_exists:
+                self._file_list.append(file)
+
+        self._model.init_inference()
+
+    def run_annotation(self):
+        """ Run the annotation itself, will annotation few images
+            every run to give the user the possibility to stop
+        """
+
+        max_index = min(self._current_image_idx + self._num_images_to_process, len(self._file_list))
+        for image_file in self._file_list[self._current_image_idx: max_index]:
+            print('Processing image', image_file)
+
+            radius_x = int(self._trigger_fov[0]/2.)
+            radius_y = int(self._trigger_fov[1]/2.)
+
+            # Pad image to be able to slide the triggerbot over the entire image
+            image = Image.open(os.path.join(self._image_path, image_file))
+            padded_image = Image.new(image.mode, (image.width + self._trigger_fov[1],
+                                                  image.height + self._trigger_fov[0]), (0, 0, 0))
+            padded_image.paste(image, (radius_x, radius_y))
+
+            # Collect the x (height) and y (width) CENTER coordinates
+            step_size_x = 8  # Seems to be a good compromise of performance vs. detection quality
+            step_size_y = 4  # Seems to be a good compromise of performance vs. detection quality
+            center_x_list = np.arange(0, self._aimbot_training_fov[0] + self._step_size_height, self._step_size_height)
+            center_y_list = np.arange(0, self._aimbot_training_fov[1] - self._step_size_width, self._step_size_width)
+            # Empty matrix to collect the triggerbot response
+            output = np.zeros((self._aimbot_training_fov[0],
+                               self._aimbot_training_fov[1]))
+
+            for x in center_x_list:
+                for y in center_y_list:
+                    # Crop trigger bot fov
+                    crop = np.array(padded_image.crop((y - radius_y + radius_y, x - radius_x + radius_x,
+                                                       y + radius_y + radius_y, x + radius_x + radius_x)))
+
+                    # And predict whether we are on target
+                    if self._model.predict_is_on_target(np.expand_dims(crop, axis=0)):
+                        half_step_height = int(self._step_size_height/2.)
+                        half_step_width = int(self._step_size_width/2.)
+                        output[x-half_step_height:x+half_step_height, y-half_step_width:y+half_step_width] = 255
+
+            # Find contours in response map
+            cnts, hierarchy = cv.findContours(output.astype(np.uint8), cv.RETR_EXTERNAL,
+                                              cv.CHAIN_APPROX_SIMPLE)
+            # And create bounding boxes for them
+            bounding_boxes = [cv.boundingRect(c) for c in cnts]
+
+            # Switch to uint8, opencv needs it
+            image = np.array(image).astype(np.uint8)
+
+            # Filter out very small boxes
+            filtered_boxes = []
+            for box in bounding_boxes:
+                if box[2] >= 2 * step_size_y and box[3] >= 2 * step_size_x:
+                    filtered_boxes.append(box)
+
+            # Create annotation files
+            if filtered_boxes:
+                anno_file_name = os.path.join(self._image_path, image_file.split('.')[0] + "_anno.txt")
+                with open(anno_file_name, 'w') as file:
+                    for box in filtered_boxes:
+                        # Left, Top, Right, Bottom
+                        file.write(str(box[0]) + ' ' + str(box[1]) + ' ' + str(box[2]) + ' ' + str(box[3]) + '\n')
+            # If there are none, remove the file
+            else:
+                os.remove(os.path.join(self._image_path, image_file))
+
+            # And finally save the annotated image for manual inspection
+            for box in filtered_boxes:
+                cv.rectangle(image, (int(box[0]), int(box[1])),
+                             (int(box[0] + box[2]), int(box[1] + box[3])), (0, 255, 0), 2)
+
+            cv.imwrite(os.path.join(self._annotated_image_path, image_file), image)
+            self._current_image_idx += 1
+
+    def shutdown_annotation(self):
+        """ Shutdown the trigger model for annotation """
+
+        self._model.shutdown_inference()
 
 
 def _get_latest_inc(path):
--- quake_ai/core/trigger_bot_inference.py
+++ quake_ai/core/trigger_bot_inference.py
@@ -78,3 +78,4 @@ class TriggerBot:
         self._model.shutdown_inference()
 
 
+
