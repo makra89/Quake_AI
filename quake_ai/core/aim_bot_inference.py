f062976a14a16a28439011e1e858f473891afb68
Manuel Kraus <manuel.mk.kraus@gmail.com>
2021-09-15 14:14:29 +0200

    Switch to pygame overlay

--- quake_ai/core/aim_bot_inference.py
+++ quake_ai/core/aim_bot_inference.py
@@ -31,12 +31,13 @@
 import numpy as np
 import keyboard
 import pydirectinput
-import tkinter as tk
 import win32gui
 import win32con
-
-
+import win32api
+import pygame
+from ctypes import windll
 from quake_ai.utils.aimbot_model import AimbotModel
+import time
 
 
 class Aimbot:
@@ -82,7 +83,6 @@ class Aimbot:
         """ Run the trigger bot for one screenshot """
 
         if self._active:
-
             screenshot = np.array(self._screenshot_func())
             boxes, scores, classes, nums = self._model.predict(screenshot)
 
@@ -112,13 +112,13 @@ class Aimbot:
                 if dist < current_min_dist:
                     current_min_dist = dist
                     current_shortest_rel = (int(rel_x), int(rel_y))
-                    current_top_left = (left, top)
-                    current_bottom_right = (left + width, top + height)
+                    current_left_top = (left, top)
+                    current_width_height = (width, height)
                     found = True
 
             if found:
-                pydirectinput.moveRel(current_shortest_rel[0], current_shortest_rel[1], 0.005, relative=True)
-                self._overlay.activate_rectangle(current_top_left, current_bottom_right)
+                pydirectinput.move(current_shortest_rel[0], current_shortest_rel[1])
+                self._overlay.activate_rectangle(current_left_top, current_width_height)
             else:
                 self._overlay.deactivate_rectangle()
 
@@ -135,7 +135,6 @@ class Aimbot:
         if self._activation_hook is not None:
             keyboard.unhook(self._activation_hook)
 
-        self._overlay.destroy()
         self._overlay = None
 
     def _activate_aimbot(self, _):
@@ -149,63 +148,47 @@ class Overlay:
 
         self._rect_active = False
 
-        self._root = tk.Tk()
-        self._root.title("Overlay")
+        pygame.init()
 
-        # get screen width and height
-        ws = self._root.winfo_screenwidth()  # width of the screen
-        hs = self._root.winfo_screenheight()  # height of the screen
+        # Create transparent, clickthrough window
+        self._window = pygame.display.set_mode((fov[1], fov[0]), pygame.NOFRAME)
+        self._trans_color = (255, 255, 255)  # Transparency color
+        self._window.fill(self._trans_color)
+        self._hwnd = pygame.display.get_wm_info()["window"]
+        _set_transparent_clickthrough(self._hwnd, self._trans_color, int(aim_pos[0] - fov[1]/2.),
+                                      int(aim_pos[1] - fov[0]/2.))
 
-        # set the dimensions of the screen
-        # and where it is placed
-        self._root.geometry('%dx%d+%d+%d' % (fov[1], fov[0], aim_pos[0] - fov[1]/2.,
-                                             aim_pos[1] - fov[0]/2.))
+        self._rect = pygame.Rect(50, 50, 50, 50)
+        self._rect_color = self._trans_color
 
-        self._root.lift()
-        self._root.wm_attributes("-topmost", True)
-        self._root.wm_attributes("-disabled", True)
-        self._root.overrideredirect(1)  # Remove border
-        self._root.wm_attributes("-transparentcolor", "white")
+        self.update()
 
-        _set_clickthrough('Overlay')
+    def activate_rectangle(self, left_top, width_heigth):
 
-        self._canvas = tk.Canvas(self._root, bd=0, highlightthickness=0)
-        self._canvas.config(bg='white')
-        self._rect = self._canvas.create_rectangle(0, 0, 0, 0,
-                                                   outline="#f11", width=2)
-        self._canvas.itemconfigure(self._rect, state='hidden')
-        self._canvas.pack(fill=tk.BOTH, expand=1)
-        self._root.update()
-
-    def activate_rectangle(self, top_left, bottom_right):
-
-        self._rect_active = True
-        self._canvas.coords(self._rect, top_left[0], top_left[1], bottom_right[0], bottom_right[1])
-        if self._rect_active:
-            self._canvas.itemconfigure(self._rect, state='normal')
+        self._rect = pygame.Rect(left_top, width_heigth)
+        self._rect_color = (255, 0, 0)
 
     def deactivate_rectangle(self):
 
-        if self._rect_active:
-            self._canvas.itemconfigure(self._rect, state='hidden')
-        self._rect_active = False
+        self._rect_color = self._trans_color
 
     def update(self):
 
-        self._root.update()
+        self._window.fill(self._trans_color)
+        pygame.draw.rect(self._window, self._rect_color, self._rect, 3)
+        pygame.display.flip()
+        pygame.event.pump()
+        time.sleep(0.01)
 
-    def destroy(self):
 
-        self._root.destroy()
-
-
-def _set_clickthrough(window_name):
+def _set_transparent_clickthrough(hwnd, color, x_pos, y_pos):
     """ Define as transparent, clickthrough window """
 
-    hwnd = win32gui.FindWindow(None, window_name)
     styles = win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE)
     styles |= win32con.WS_EX_LAYERED | win32con.WS_EX_TRANSPARENT
     win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, styles)
+    win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*color), 0, win32con.LWA_COLORKEY)
+    windll.user32.SetWindowPos(hwnd, -1, x_pos, y_pos, 0, 0, 0x0001)
 
 
 
