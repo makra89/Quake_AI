bcbb066f537c1e3d76ad48667d89761f48c7d527
Manuel Kraus <manuel.mk.kraus@gmail.com>
2021-09-17 14:12:24 +0200

    Also slightly change the color during image augmentation

--- quake_ai/utils/aimbot_model.py
+++ quake_ai/utils/aimbot_model.py
@@ -136,10 +136,16 @@ class TrainableAimbotModel(AimbotModel):
             iaa.Crop(percent=(0, 0.1)),  # random crops
             # Strengthen or weaken the contrast in each image.
             iaa.LinearContrast((0.75, 1.5)),
-            # Add gaussian noise. Preserves color!
-            iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05 * 255), per_channel=False),
-            # Make some images brighter and some darker. Preserves color!
-            iaa.Multiply((0.8, 1.2), per_channel=False),
+            # Add gaussian noise.
+            # For 50% of all images, we sample the noise once per pixel.
+            # For the other 50% of all images, we sample the noise per pixel AND
+            # channel. This can change the color (not only brightness) of the
+            # pixels.
+            iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05 * 255), per_channel=0.5),
+            # Make some images brighter and some darker.
+            # In 20% of all cases, we sample the multiplier once per channel,
+            # which can end up changing the color of the images.
+            iaa.Multiply((0.8, 1.2), per_channel=0.2),
             # Apply affine transformations to each image.
             # Scale/zoom them, translate/move them, rotate them and shear them.
             iaa.Affine(
